9137a6266230310c949cf52d7c445aa516c9b233
Juan Antonio Galan <juan.antonio.galan@instoresolutions.com>
2010-08-31 23:34:44 +0200

    Added fields with alias to pendingFields array, fixed DC-585

--- lib/Doctrine/Query.php
+++ lib/Doctrine/Query.php
@@ -484,7 +484,7 @@ class Doctrine_Query extends Doctrine_Query_Abstract implements Countable
         }
 
         $sql = array();
-        foreach ($fields as $fieldName) {
+        foreach ($fields as $fieldAlias => $fieldName) {
             $columnName = $table->getColumnName($fieldName);
             if (($owner = $table->getColumnOwner($columnName)) !== null &&
                     $owner !== $table->getComponentName()) {
@@ -496,10 +496,17 @@ class Doctrine_Query extends Doctrine_Query_Abstract implements Countable
                        . ' AS '
                        . $this->_conn->quoteIdentifier($tableAlias . '__' . $columnName);
             } else {
-                $columnName = $table->getColumnName($fieldName);
+                // Fix for http://www.doctrine-project.org/jira/browse/DC-585
+                // Take the field alias if available
+                if (isset($this->_aggregateAliasMap[$fieldAlias])) {
+                    $aliasSql = $this->_aggregateAliasMap[$fieldAlias];
+                } else {
+                    $columnName = $table->getColumnName($fieldName);
+                    $aliasSql = $this->_conn->quoteIdentifier($tableAlias . '__' . $columnName);
+                }
                 $sql[] = $this->_conn->quoteIdentifier($tableAlias) . '.' . $this->_conn->quoteIdentifier($columnName)
                        . ' AS '
-                       . $this->_conn->quoteIdentifier($tableAlias . '__' . $columnName);
+                       . $aliasSql;
             }
         }
 
@@ -651,6 +658,13 @@ class Doctrine_Query extends Doctrine_Query_Abstract implements Countable
                 $this->_queryComponents[$componentAlias]['agg'][$index] = $alias;
 
                 $this->_neededTables[] = $tableAlias;
+
+                // Fix for http://www.doctrine-project.org/jira/browse/DC-585
+                // Add selected columns to pending fields
+                if (preg_match('/^([^\(]+)\.(\'?)(.*?)(\'?)$/', $expression, $field)) {
+                    $this->_pendingFields[$componentAlias][$alias] = $field[3];
+                }
+
             } else {
                 $e = explode('.', $terms[0]);
 

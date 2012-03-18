75ab73ee3295d996f91919004e3c573e0044f75f
jerome <jerome.macias@gmail.com>
2012-03-19 00:06:28 +0100

    Fixes E_NOTICE when using array_diff with PHP 5.4 (patch from @fixe)

--- lib/Doctrine/Query/Abstract.php
+++ lib/Doctrine/Query/Abstract.php
@@ -211,7 +211,7 @@ abstract class Doctrine_Query_Abstract
      * @var string
      */
     protected $_rootAlias = '';
-	
+
     /**
      * @var integer $type                   the query type
      *
@@ -509,7 +509,7 @@ abstract class Doctrine_Query_Abstract
     public function getFlattenedParams($params = array())
     {
         return array_merge(
-            (array) $params, (array) $this->_params['exec'], 
+            (array) $params, (array) $this->_params['exec'],
             $this->_params['join'], $this->_params['set'],
             $this->_params['where'], $this->_params['having']
         );
@@ -534,7 +534,7 @@ abstract class Doctrine_Query_Abstract
     {
         $this->_params = $params;
     }
-    
+
     /**
      * getCountQueryParams
      * Retrieves the parameters for count query
@@ -562,19 +562,19 @@ abstract class Doctrine_Query_Abstract
     public function fixArrayParameterValues($params = array())
     {
         $i = 0;
-	
+
         foreach ($params as $param) {
             if (is_array($param)) {
                 $c = count($param);
 
                 array_splice($params, $i, 1, $param);
-                
+
                 $i += $c;
             } else {
                 $i++;
             }
         }
-        
+
         $this->_execParams = $params;
     }
 
@@ -639,17 +639,17 @@ abstract class Doctrine_Query_Abstract
             $tableAlias .= '.';
         }
 
-        // Fix for 2015: loop through whole inheritanceMap to add all   
-        // keyFields for inheritance (and not only the first) 
-        $retVal = ""; 
-        $count = 0; 
-         
-        foreach ($map as $field => $value) { 
+        // Fix for 2015: loop through whole inheritanceMap to add all
+        // keyFields for inheritance (and not only the first)
+        $retVal = "";
+        $count = 0;
+
+        foreach ($map as $field => $value) {
             if ($count++ > 0) {
                 $retVal .= ' AND ';
             }
 
-            $identifier = $this->_conn->quoteIdentifier($tableAlias . $field); 
+            $identifier = $this->_conn->quoteIdentifier($tableAlias . $field);
             $retVal .= $identifier . ' = ' . $this->_conn->quote($value);
         }
 
@@ -784,7 +784,7 @@ abstract class Doctrine_Query_Abstract
         if ( ! $this->_queryComponents) {
             $this->getSqlQuery(array(), false);
         }
-        
+
         return $this->_rootAlias;
     }
 
@@ -938,13 +938,13 @@ abstract class Doctrine_Query_Abstract
                 if ($cached) {
                     // Rebuild query from cache
                     $query = $this->_constructQueryFromCache($cached);
-                    
+
                     // Assign building/execution specific params
                     $this->_params['exec'] = $params;
-            
+
                     // Initialize prepared parameters array
                     $this->_execParams = $this->getFlattenedParams();
-                    
+
                     // Fix possible array parameter values in SQL params
                     $this->fixArrayParameterValues($this->getInternalParams());
                 } else {
@@ -967,7 +967,7 @@ abstract class Doctrine_Query_Abstract
         } else {
             $query = $this->_view->getSelectSql();
         }
-        
+
         // Get prepared SQL params for execution
         $params = $this->getInternalParams();
 
@@ -1038,7 +1038,7 @@ abstract class Doctrine_Query_Abstract
                 $this->_hydrator->setQueryComponents($this->_queryComponents);
                 if ($this->_type == self::SELECT && $hydrationMode == Doctrine_Core::HYDRATE_ON_DEMAND) {
                     $hydrationDriver = $this->_hydrator->getHydratorDriver($hydrationMode, $this->_tableAliasMap);
-                    $result = new Doctrine_Collection_OnDemand($stmt, $hydrationDriver, $this->_tableAliasMap); 
+                    $result = new Doctrine_Collection_OnDemand($stmt, $hydrationDriver, $this->_tableAliasMap);
                 } else {
                     $result = $this->_hydrator->hydrateResultSet($stmt, $this->_tableAliasMap);
                 }
@@ -1055,7 +1055,7 @@ abstract class Doctrine_Query_Abstract
      * Blank template method free(). Override to be used to free query object memory
      */
     public function free()
-    { 
+    {
     }
 
     /**
@@ -1149,7 +1149,7 @@ abstract class Doctrine_Query_Abstract
         $copy->free();
 
         if ($componentsBefore !== $componentsAfter) {
-            return array_diff($componentsAfter, $componentsBefore);
+            return array_diff_assoc($componentsAfter, $componentsBefore);
         } else {
             return $componentsAfter;
         }
@@ -1183,9 +1183,9 @@ abstract class Doctrine_Query_Abstract
         foreach ($cachedComponents as $alias => $components) {
             $e = explode('.', $components['name']);
             if (count($e) === 1) {
-                $manager = Doctrine_Manager::getInstance(); 
-                if ( ! $this->_passedConn && $manager->hasConnectionForComponent($e[0])) { 
-                    $this->_conn = $manager->getConnectionForComponent($e[0]); 
+                $manager = Doctrine_Manager::getInstance();
+                if ( ! $this->_passedConn && $manager->hasConnectionForComponent($e[0])) {
+                    $this->_conn = $manager->getConnectionForComponent($e[0]);
                 }
                 $queryComponents[$alias]['table'] = $this->_conn->getTable($e[0]);
             } else {
@@ -1491,7 +1491,7 @@ abstract class Doctrine_Query_Abstract
     /**
      * Adds conditions to the HAVING part of the query.
      *
-     * This methods add HAVING clauses. These clauses are used to narrow the 
+     * This methods add HAVING clauses. These clauses are used to narrow the
      * results by operating on aggregated values.
      * <code>
      * $q->having('num_phonenumbers > ?', 1);

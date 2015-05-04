0606aece89db448dfbf5f44b9374d4e0e1d5bd76
Maciej Holyszko <14310995+falkenhawk@users.noreply.github.com>
2015-05-04 14:57:36 +0000

    (doctrine-1.2.x) fixes for failing test cases caused by previous mysql varchar limit change

--- tests/Export/RecordTestCase.php
+++ tests/Export/RecordTestCase.php
@@ -49,7 +49,7 @@ class Doctrine_Export_Record_TestCase extends Doctrine_UnitTestCase
     {
         $sql = $this->conn->export->exportClassesSql(array('ForeignKeyTest'));
 
-        $this->assertEqual($sql[0], 'CREATE TABLE foreign_key_test (id BIGINT AUTO_INCREMENT, name TEXT, code INT, content TEXT, parent_id BIGINT, INDEX parent_id_idx (parent_id), PRIMARY KEY(id)) ENGINE = INNODB');
+        $this->assertEqual($sql[0], 'CREATE TABLE foreign_key_test (id BIGINT AUTO_INCREMENT, name TEXT, code INT, content VARCHAR(4000), parent_id BIGINT, INDEX parent_id_idx (parent_id), PRIMARY KEY(id)) ENGINE = INNODB');
         if (isset($sql[1])) {
             $this->assertEqual($sql[1], 'ALTER TABLE foreign_key_test ADD CONSTRAINT foreign_key_test_parent_id_foreign_key_test_id FOREIGN KEY (parent_id) REFERENCES foreign_key_test(id) ON UPDATE RESTRICT ON DELETE CASCADE');
         } else {
@@ -61,7 +61,7 @@ class Doctrine_Export_Record_TestCase extends Doctrine_UnitTestCase
     {
         $sql = $this->conn->export->exportClassesSql(array('MysqlIndexTestRecord'));
 
-        $this->assertEqual($sql[0], 'CREATE TABLE mysql_index_test_record (id BIGINT AUTO_INCREMENT, name TEXT, code INT, content TEXT, FULLTEXT INDEX content_idx (content), UNIQUE INDEX namecode_idx (name, code), PRIMARY KEY(id)) ENGINE = MYISAM');
+        $this->assertEqual($sql[0], 'CREATE TABLE mysql_index_test_record (id BIGINT AUTO_INCREMENT, name TEXT, code INT, content VARCHAR(4000), FULLTEXT INDEX content_idx (content), UNIQUE INDEX namecode_idx (name, code), PRIMARY KEY(id)) ENGINE = MYISAM');
     }
 
     public function testRecordDefinitionsSupportTableOptions()
@@ -75,7 +75,7 @@ class Doctrine_Export_Record_TestCase extends Doctrine_UnitTestCase
     {
         $sql = $this->conn->export->exportClassesSql(array('ForeignKeyTest'));
 
-        $this->assertEqual($sql[0], 'CREATE TABLE foreign_key_test (id BIGINT AUTO_INCREMENT, name TEXT, code INT, content TEXT, parent_id BIGINT, INDEX parent_id_idx (parent_id), PRIMARY KEY(id)) ENGINE = INNODB');
+        $this->assertEqual($sql[0], 'CREATE TABLE foreign_key_test (id BIGINT AUTO_INCREMENT, name TEXT, code INT, content VARCHAR(4000), parent_id BIGINT, INDEX parent_id_idx (parent_id), PRIMARY KEY(id)) ENGINE = INNODB');
         if (isset($sql[1])) {
             $this->assertEqual($sql[1], 'ALTER TABLE foreign_key_test ADD CONSTRAINT foreign_key_test_parent_id_foreign_key_test_id FOREIGN KEY (parent_id) REFERENCES foreign_key_test(id) ON UPDATE RESTRICT ON DELETE CASCADE');
         } else {
@@ -100,7 +100,7 @@ class Doctrine_Export_Record_TestCase extends Doctrine_UnitTestCase
         Doctrine_Core::createTablesFromModels(dirname(__FILE__) . DIRECTORY_SEPARATOR .'..' . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . 'export');
         $this->assertEqual($this->adapter->pop(), 'COMMIT');
         $this->assertEqual($this->adapter->pop(), 'ALTER TABLE cms__category_languages ADD CONSTRAINT cms__category_languages_category_id_cms__category_id FOREIGN KEY (category_id) REFERENCES cms__category(id) ON DELETE CASCADE');
-        $this->assertEqual($this->adapter->pop(), 'CREATE TABLE cms__category_languages (id BIGINT AUTO_INCREMENT, name TEXT, category_id BIGINT, language_id BIGINT, INDEX index_category_idx (category_id), INDEX index_language_idx (language_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = INNODB');
+        $this->assertEqual($this->adapter->pop(), 'CREATE TABLE cms__category_languages (id BIGINT AUTO_INCREMENT, name VARCHAR(256), category_id BIGINT, language_id BIGINT, INDEX index_category_idx (category_id), INDEX index_language_idx (language_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = INNODB');
         $this->assertEqual($this->adapter->pop(), 'CREATE TABLE cms__category (id BIGINT AUTO_INCREMENT, created DATETIME, parent BIGINT, position MEDIUMINT, active BIGINT, INDEX index_parent_idx (parent), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = INNODB');   
         $this->assertEqual($this->adapter->pop(), 'BEGIN TRANSACTION');
     }
